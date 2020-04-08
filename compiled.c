8d96226c38e9478afd4c8cea1d4d3a26beecf8e2
yixin0829 <yixin0829@outlook.com>
2020-04-08 18:47:44 -0400

    Drawing is partially working with issue like reading speed from FIFO is sometimes slow and where to set prev_location

--- compiled.c
+++ compiled.c
@@ -246,9 +246,8 @@ int main(void){
     
   while(true){
 
-    //Read from KEY port to see if ANY KEY is pressed
+    // Read from KEY port to see if ANY KEY is pressed
     reset = *RESET_BUTTON_PTR;
-
     if(reset != 0){ //if a KEY is pressed
         printf("a button is pressed, resetting...\n");
         //Reset screen of current back buffer
@@ -267,23 +266,29 @@ int main(void){
         continue;
     }
 
-    //@ Getting mouse_movement struct 
+    // Getting mouse_movement struct 
     mouse_movement movement = get_mouse_movement();
     bool left_clicked = movement.left_pressed_bit;
     bool right_clicked = movement.right_pressed_bit;
     
     
-    //@ Erase by drawing WHITE on the last last previous location (); set left click to 0 to avoid write 1 to canavas
-    draw_cursor(prev_buffer_cursor_location[0], prev_buffer_cursor_location[1], WHITE, cursor_size, 0);
+    //@ Erasing - by drawing WHITE on the last last previous location (); set left click to 0 to avoid write 1 to canavas
+    if(!left_clicked){
+      draw_cursor(prev_buffer_cursor_location[0], prev_buffer_cursor_location[1], WHITE, cursor_size, 0);
+    } else {
+      draw_cursor(prev_buffer_cursor_location[0], prev_buffer_cursor_location[1], cursor_colour, cursor_size, 0);
+    }
 
-    //@ Redraw the after erasing white avoid drawing white on brush icons and brush size blocks
+    //@ Redraw to refresh after erasing white avoid drawing white on brush icons and brush size blocks
     redraw_icon_and_box();
 
-    // Store current cursor location to previous location before add with movement
+    //@ Updating cursor colour/size and boundary check for cursor movement
+    // Store cursor location (the location previously used to draw in the back buffer - now it's front) to previous location before add mouse movement
+    // The prev_location will be used in next cycle to erase back buffer - now it's front 
     prev_buffer_cursor_location[0] = cursor_location[0];
     prev_buffer_cursor_location[1] = cursor_location[1];
 
-    // Update cursor_location
+    // Update cursor_location (location used to draw on the current back buffer)
     cursor_location[0] = cursor_location[0] + movement.dx;
     cursor_location[1] = cursor_location[1] + movement.dy;
 
@@ -293,13 +298,35 @@ int main(void){
     //Boundary checking - If left clicking drawing - can only move within the textbox (correct cursor location if exceeds the bounddary)
     boundary_check(cursor_location);
 
+    // Add a delay of 0.5 second 
     counting_down();
 
-    //Drawing cursor on updated location (on the back buffer)
-    draw_cursor(cursor_location[0], cursor_location[1], cursor_colour, cursor_size, left_clicked);
+    //@ Drawing cursor on updated location (on the back buffer)
+    //If not drawing - Only need to draw cursor on current back buffer
+    if(!left_clicked) {
+      draw_cursor(cursor_location[0], cursor_location[1], cursor_colour, cursor_size, left_clicked);
+    } else { // The User is drawing on canvas - Need drawback and then swap to draw on both back&front buffer to keep the "INK"
+      
+      draw_cursor(cursor_location[0], cursor_location[1], cursor_colour, cursor_size, left_clicked);
+      printf(" 1) x/y: %i, %i\n", cursor_location[0], cursor_location[1]); 
+      // Swaping front buffer to back
+      wait_for_vsync(); // swap front and back buffers on VGA vertical sync i.e. display drawed back buffer 
+      pixel_buffer_start = *(pixel_ctrl_ptr + 1); // Set to draw on new back buffer
+      // Draw on current back buffer (which is front before)
+      draw_cursor(cursor_location[0], cursor_location[1], cursor_colour, cursor_size, left_clicked);
+      printf(" 2) x/y: %i, %i\n", cursor_location[0], cursor_location[1]);
+      wait_for_vsync(); // swap front and back buffers on VGA vertical sync i.e. display drawed back buffer 
+      pixel_buffer_start = *(pixel_ctrl_ptr + 1); // Set to draw on new back buffer
+
+      //? Set prev_location outside of the canvas to avoiding erasing on drawed pixels
+      prev_buffer_cursor_location[0] = 150;
+      prev_buffer_cursor_location[1] = 150;
+
+    }
 
 
-    //!DEBUG
+
+    //! DEBUG
     if(left_clicked) {
       printf(" x/y: %i, %i\n", cursor_location[0], cursor_location[1]);
     }
@@ -594,61 +621,61 @@ mouse_movement get_mouse_movement() {
 
     while(1) {
     PS2_data = *(PS2_ptr);	// read the Data register in the PS/2 port
-		
-		RVALID = (PS2_data & 0x8000);	// extract the RVALID field (15th bit)
-		if (RVALID && counter == 1) {
-			// &0xFF: Only take the first 8 bits - data
-			byte1 = PS2_data & 0xFF; 
-		} else if (RVALID && counter == 2) {
-			byte2 = PS2_data & 0xFF;
-		} else if (RVALID && counter == 3) {
-			byte3 = PS2_data & 0xFF;
-		}
+    
+    RVALID = (PS2_data & 0x8000);	// extract the RVALID field (15th bit)
+    if (RVALID && counter == 1) {
+      // &0xFF: Only take the first 8 bits - data
+      byte1 = PS2_data & 0xFF; 
+    } else if (RVALID && counter == 2) {
+      byte2 = PS2_data & 0xFF;
+    } else if (RVALID && counter == 3) {
+      byte3 = PS2_data & 0xFF;
+    }
 
 
 
-		if (counter ==3) { // One 3-byte movement packet has been stored in byte1/2/3, returning...
-			//Get dx/dy movement
-			int x_sign_bit = byte1 & 0x10; //0b00010000
-			int y_sign_bit = byte1 & 0x20; //0b00100000
-			int left_pressed_bit = byte1 & 0x01;// read 1st bit of the 1st byte of 3-byte movement packet
+    if (counter ==3) { // One 3-byte movement packet has been stored in byte1/2/3, returning...
+      //Get dx/dy movement
+      int x_sign_bit = byte1 & 0x10; //0b00010000
+      int y_sign_bit = byte1 & 0x20; //0b00100000
+      int left_pressed_bit = byte1 & 0x01;// read 1st bit of the 1st byte of 3-byte movement packet
             int right_pressed_bit = byte1 & 0x02; // read 2nd bit of the 1st byte of 3-byte movement packet
-			
-			//Constructing movement struct element
-			movement.left_pressed_bit = left_pressed_bit;
-            movement.right_pressed_bit = right_pressed_bit;
-			
-			if(x_sign_bit == 0) { //positive dx
-				movement.dx = byte2;
-			} else { //negative dx with sign extension
-				int sign_extended_twos =   0xFFFFFF00 + byte2 ; // Equivalent to 0xFFFFFE00 + byte 2
-				movement.dx = sign_extended_twos;
-			}
+      
+      //Constructing movement struct element
+      movement.left_pressed_bit = left_pressed_bit;
+      movement.right_pressed_bit = right_pressed_bit;
+      
+      if(x_sign_bit == 0) { //positive dx
+        movement.dx = byte2;
+      } else { //negative dx with sign extension
+        int sign_extended_twos =   0xFFFFFF00 + byte2 ; // Equivalent to 0xFFFFFE00 + byte 2
+        movement.dx = sign_extended_twos;
+      }
 
-			if(y_sign_bit == 0) { //positive dx
-				movement.dy = byte3;
-			} else { //negative dx
-				int sign_extended_twos = 0xFFFFFF00 + byte3;
-				movement.dy = sign_extended_twos;
-			}
-			
-			// printf("x_movement byte (byte 2) is %i \n", movement.dx);
-			// printf("y_movement byte (byte 3) is %i \n", movement.dy);
-			// printf("the left-pressed bit is %i \n", left_pressed_bit);
+      if(y_sign_bit == 0) { //positive dx
+        movement.dy = byte3;
+      } else { //negative dx
+        int sign_extended_twos = 0xFFFFFF00 + byte3;
+        movement.dy = sign_extended_twos;
+      }
+      
+      // printf("x_movement byte (byte 2) is %i \n", movement.dx);
+      // printf("y_movement byte (byte 3) is %i \n", movement.dy);
+      // printf("the left-pressed bit is %i \n", left_pressed_bit);
 
-			return movement;
+      return movement;
 
-		} else { // Continue reading unfinished 3-byte packet
-			counter ++;
-		}
+    } else { // Continue reading unfinished 3-byte packet
+      counter ++;
+    }
 
-		if ( (byte2 == 0xAA) && (byte3 == 0x00) )
-		{
-			// mouse inserted; initialize sending of data
-			*(PS2_ptr) = 0xF4;
-		}
-		
-		
+    if ( (byte2 == 0xAA) && (byte3 == 0x00) )
+    {
+      // mouse inserted; initialize sending of data
+      *(PS2_ptr) = 0xF4;
+    }
+    
+    
     }
 
 }
